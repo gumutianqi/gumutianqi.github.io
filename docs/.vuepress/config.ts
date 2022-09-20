9480da643c341151c3b5513cb27c6b1db093366e
阿古 <gujianguo@meiqia.com>
2022-09-20 16:06:12 +0800

    feat add page first-know-tektoncd

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,105 +1,90 @@
-import type { DefaultThemeOptions } from 'vuepress'
-import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from "vuepress";
+import { defineUserConfig } from "vuepress";
 
 export default defineUserConfig<DefaultThemeOptions>({
-  lang: 'zh-CN',
-  title: '古拉里 学架构',
-  description: 'Just Do IT',
-  head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
+  lang: "zh-CN",
+  title: "古拉里 学架构",
+  description: "Just Do IT",
+  head: [["link", { rel: "icon", href: "/images/favicon.ico" }]],
 
   themeConfig: {
-    logo: '/images/logo.png',
+    logo: "/images/logo.png",
     smoothScroll: true,
     contributors: false,
-    lastUpdatedText: '上次更新',
+    lastUpdatedText: "上次更新",
     navbar: [
       {
-        text: '首页',
-        link: '/'
+        text: "首页",
+        link: "/",
       },
       {
-        text: 'GitHub',
-        link: 'https://github.com/gumutianqi'
+        text: "GitHub",
+        link: "https://github.com/gumutianqi",
       },
       {
-        text: '讨论区',
-        link: '/board.md'
-      }
+        text: "讨论区",
+        link: "/board.md",
+      },
     ],
     sidebarDepth: 0,
     sidebar: [
       {
-        text: '目录',
-        link: '/SUMMARY.md'
+        text: "目录",
+        link: "/SUMMARY.md",
       },
       {
-        text: '前言',
+        text: "前言",
+        children: ["/introduction/about-me.md"],
+      },
+      {
+        text: "读书笔记",
+        children: ["/reading/about-reading.md"],
+      },
+      {
+        text: "云原生",
+        children: ["/cloud-native/abount-cloud-native.md"],
+      },
+      {
+        text: "最佳实践",
         children: [
-          '/introduction/about-me.md'
+          {
+            text: "聊聊数据",
+            children: ["/best-practice/talk-about-data/try-to-tapdata.md"],
+          },
+          {
+            text: "微服务",
+            children: ["/best-practice/microservice/about-microservice.md"],
+          },
+          {
+            text: "Serverless",
+            children: ["/best-practice/serverless/about-serverless.md"],
+          },
+          {
+            text: "RFC 日常",
+          },
         ],
       },
       {
-        text: '读书笔记',
-        children: [
-          "/reading/about-reading.md"
-        ]
-      },
-      {
-        text: '云原生',
-        children: [
-          "/cloud-native/abount-cloud-native.md"
-        ]
-      },
-      {
-        text: '最佳实践',
-        children: [
-          {
-            text: '聊聊数据',
-            children: [
-              '/best-practice/talk-about-data/try-to-tapdata.md',
-            ]
-          },
-          {
-            text: '微服务',
-            children: [
-              "/best-practice/microservice/about-microservice.md"
-            ]
-          },
-          {
-            text: 'Serverless',
-            children: [
-              "/best-practice/serverless/about-serverless.md"
-            ]
-          },
-          {
-            text: 'RFC 日常',
-          }
-        ]
-      },
-      {
-        text: '随笔',
-        children: [
-          '/tricks/about-life.md',
-          '/tricks/hhkb-pro2-types-show.md'
-        ]
+        text: "随笔",
+        children: ["/tricks/about-life.md", "/tricks/hhkb-pro2-types-show.md"],
       },
     ],
   },
   plugins: [
     [
-      '@vuepress/docsearch',
+      "@vuepress/docsearch",
       {
-        apiKey: '<API_KEY>',
-        indexName: '<INDEX_NAME>',
+        apiKey: "<API_KEY>",
+        indexName: "<INDEX_NAME>",
         locales: {
-          '/': {
-            placeholder: 'Search Documentation',
+          "/": {
+            placeholder: "Search Documentation",
           },
-          '/zh/': {
-            placeholder: '搜索文档',
+          "/zh/": {
+            placeholder: "搜索文档",
           },
         },
       },
-    ]
-  ]
-})
+    ],
+  ],
+});
---
+++ docs/cloud-native/first-know-tektoncd.md
@@ -0,0 +1,16 @@
+---
+lang: zh-CN
+title: 初识 TektonCD
+description: 初识 TektonCD(First Know Tekton-CD)
+---
+
+## 初识 TektonCD
+
+![visitors](https://visitor-badge.glitch.me/badge?page_id=gumutianqi_github_io-first_know_tektoncd)
+
+
+1. Create a Kubernetes cluster.
+2. Install Tekton pipelines.
+3. Create a Task.
+4. Use TaskRun to instantiate and run your Task.
+
